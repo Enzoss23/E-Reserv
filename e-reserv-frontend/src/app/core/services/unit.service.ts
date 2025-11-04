dba0c4dfe017c67a17e64b7fde485ba8fcffccee
Enzoss23 <enzoss23@gmail.com>
2025-11-03 21:29:14 -0300

    Conexão com o Backend

--- e-reserv-frontend/src/app/core/services/unit.service.ts
+++ e-reserv-frontend/src/app/core/services/unit.service.ts
@@ -1,22 +1,32 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Unit } from '../models/unit.model';
-import { Observable } from 'rxjs';
+import { Observable, catchError, map, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UnitService {
-
-  private url: string = 'http://172.20.10.2:8080/unit'
+  private api = '/api/units';
+  private mock = '/assets/mock/units.json';
 
   constructor(private http: HttpClient) {}
 
   public findAll(): Observable<Unit[]> {
-    return this.http.get<Unit[]>(this.url);
+    return this.http.get<Unit[] | any>(this.api).pipe(
+      map((res: any) => (Array.isArray(res) ? (res as Unit[]) : [])),
+      catchError(() => this.http.get<Unit[]>(this.mock))
+    );
   }
 
   public findById(id: number): Observable<Unit> {
-    return this.http.get<Unit>(`${this.url}/${id}`);
+    return this.http.get<Unit | any>(`${this.api}/${id}`).pipe(
+      map((res: any) => res as Unit),
+      catchError(() =>
+        this.http.get<Unit[]>(this.mock).pipe(
+          map((list) => list.find((u: any) => Number(u.id) === Number(id)) as Unit)
+        )
+      )
+    );
   }
 }
